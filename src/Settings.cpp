b98faa12d17b44b510b84e4b9b3e36473c1642d2
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-11-13 16:07:00 -0500

    aaaaaaaa

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1,7 +1,7 @@
 #include "Settings.hpp"
 
 SettingNodeV3* MyButtonSettingV3::createNode(float width) {
-    return MyButtonSettingV3::create(this, width);
+    return MyButtonSettingV3::createNode(this, width);
 }
 
 // i had to include this cpp file because C++ is a legendary language!
