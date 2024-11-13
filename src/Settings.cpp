45e1c5bc95daac4b4dea07c24d4a93f73c639102
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-11-13 16:19:49 -0500

    too many args?

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1,10 +1,7 @@
 #include "Settings.hpp"
 
 SettingNodeV3* MyButtonSettingV3::createNode(float width) {
-	return MyButtonSettingNodeV3::create(
-		std::static_pointer_cast<MyButtonSettingV3>(shared_from_this()),
-		width
-	);
+	return MyButtonSettingNodeV3::create();
 }
 
 // i had to include this cpp file because C++ is a legendary language!
