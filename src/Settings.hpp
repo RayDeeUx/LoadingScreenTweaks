4005f8a729723c1c999baf99e6f40ec5178743d7
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-11-13 14:39:09 -0500

    .-.

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -45,8 +45,8 @@ protected:
 	bool init(std::shared_ptr<MyButtonSettingV3> setting, float width) {
         if (!SettingNodeV3::init(setting, width)) return false;
 		this->setContentSize({ width, 40.f });
-		std::string name = setting->getName();
-		m_desc = setting->getDescription();
+		std::string name = setting->getName()->value_or("Click me to open the Config Directory!");
+		m_desc = setting->getDescription()->value_or("<cy>(Hopefully.)</c>\nThis setting does literally nothing new except being a button that acts as a shortcut to open the config directory for this mod (which is kind of important for using this mod properly, because this is where you will store your \"New Best\" sound effects), because the legendary developers in the Geode Team themselves forgot to add a shortcut button that would have done this exact same thing, and I have gotten far too many folks (rightfully) asking me to find the config directory when using some of my other mods, and I hate repeating the same things over and over again, and I don't know if the next major update for Geode will also repeat this mistake where HJfod forgets to include a config directory shortcut button, so this setting and this button are here for extra insurance, so please enjoy this extra button, I guess. Anyway, by the time you're finished reading this, your device should be finished with opening the config directory; press \"Aight\" to close this window.");
 		auto theLabel = CCLabelBMFont::create(name.c_str(), "bigFont.fnt");
 
 		// copied a bit from louis ck jr
